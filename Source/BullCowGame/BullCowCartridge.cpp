40c385019da47c93be9b2f4b1e5ee54a6a510b9e
Lucas Santos <lucas.santos.silva2011@gmail.com>
2020-08-29 19:52:19 -0300

    Add basic game loop

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+using namespace std;
+
+FString HiddenWord;
+
+TArray<FString> HamsterNames = {
+    TEXT("Aloy"),
+    TEXT("Ritinha"),
+    TEXT("Stela"),
+    TEXT("Bart"),
+    TEXT("X"),
+    TEXT("Billie")
+};
+
+FString UBullCowCartridge::GetRandomHiddenWord() {
+    int32 randomIndex = FMath::RandRange(0, (HamsterNames.Num() - 1));
+    return HamsterNames[randomIndex];
+}
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    HiddenWord = GetRandomHiddenWord();
+
+    PrintLine(TEXT("Welcome to Bull Cow"));
+    PrintLine(TEXT("Qual é o hamster escondido?"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
-    
+{   
+    if (HiddenWord.Equals(Input, ESearchCase::IgnoreCase)) {
+        ClearScreen();
+        PrintLine(TEXT("Parabéns! Você encontrou o hamster escondido!"));
+
+        HiddenWord = GetRandomHiddenWord();
+        PrintLine(TEXT("Vamos tentar novamente..."));
+        PrintLine(TEXT("Qual é o hamster escondido?"));
+    } else {
+        ClearScreen();
+        PrintLine(TEXT("Tente novamente! Qual é o Hamster escondido?"));
+    }
 }
